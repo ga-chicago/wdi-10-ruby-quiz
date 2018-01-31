f9aace4619fb00607951dc6281a969dc3fa1a7eb
sergio <scrummish@gmail.com>
2018-01-31 14:22:07 -0600

    Ruby Quiz

---
+++ lab.rb
@@ -0,0 +1,26 @@
+# Create a array from the number one to a hundred. 
+# Loop through the array and puts to the console all the even numbers, 
+# if the number is odd put them into an empty array in descending order.
+
+# num = 1
+# 100.times{|num| p num}
+
+
+# createdArr = [1...100]
+
+# p created
+
+
+# 3.
+newHash = {
+	symbol1: "symbol1",
+	symbol2: "symbol2",
+	symbol3: "symbol3",
+	symbol4: "symbol4",
+	symbol5: "symbol5"
+}
+
+#4.
+newHash.each do |key|
+	p "the key name is #{key} and the value is #{}"
+end
