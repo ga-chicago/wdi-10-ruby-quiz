514fb04cc0d8371d11cf357a5a65ddb03146f90e
Ryne Laster <ryne.laster@gmail.com>
2018-01-31 14:15:50 -0600

    question 1 answered

---
+++ quiz.rb
@@ -0,0 +1,13 @@
+# Q. 1
+
+ryne = [*1..100]
+ryne.each do |num|
+	if num.even?
+		p num
+	end
+end
+
+#Got the numbers to loop and print evens, but couldn't remember how to put the odds in a separate array in descending order
+
+# Q. 2
+
