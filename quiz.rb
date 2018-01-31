ddc0bfa1752ef74ca7d542568d76d024052b273a
Anthony <anthonyjlower@gmail.com>
2018-01-31 14:22:49 -0600

    Quiz answers

---
+++ quiz.rb
@@ -0,0 +1,38 @@
+# Not working yet, I'll come back
+# def loop_through
+# 	arr = [*1..100]
+
+# 	arr.each {|num|
+# 		if num.even?
+# 			p num
+# 		else
+			# oddArr = []
+			# oddArr << num
+# 		end
+# 	}
+# 	p oddArr
+# end
+# loop_through
+
+
+alphArr = [*"a"..."z"]
+# Need to filter and then push
+vowelArr = alphArr.each {|letter|
+
+
+
+grades = {hw1: 90, hw2: 90, hw3: 85, hw4: 80, hw5: 90}
+# Need to loop through
+
+
+
+
+
+# Need to loop 
+hash_to_loop = {
+	bool: false;
+	hash_val: {hw1: 90, hw2: 90, hw3: 85, hw4: 80, hw5: 90}
+	arr: [1,2,3,4]
+}
+
+
